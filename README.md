88835a06e871a475cac55acea2ddc0a8b7319a13
TobiPeterG <github.threefold020@passmail.net>
2025-12-18 00:57:40 +0100

    Fix provider path

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ terraform {
   required_providers {
     nautobot = {
       version = "3.0.0"
-      source  = "nautobot/nautobot"
+      source  = "registry.terraform.io/nautobot/nautobot"
     }
   }
 }
--- local/provider.tf
+++ local/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     nautobot = {
       version = "3.0.0"
-      source  = "github.com/nautobot/nautobot"
+      source  = "registry.terraform.io/nautobot/nautobot"
     }
   }
 }
